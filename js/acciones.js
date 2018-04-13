da5ebd41cfc71bcd8ed01e8ebaebd0f30ad79c26
13emmanuel <ema.mq13@gmail.com>
2018-04-13 11:32:15 -0500

    4subida

--- js/acciones.js
+++ js/acciones.js
@@ -8,10 +8,10 @@ $(document).ready(function(e){
 		$('#disp table td').eq(9).text(device.version);
 		$('#disp table td').eq(11).text(device.uuid);
 		document.addEventListener("pause",function(){
-	escribehistoria ('la app se pausó');
+	escribehistoria ('la app se pauso');
 		},false);//pause
 		document.addEventListener("resume",function(){
-			escribehistoria ('la app se reinició');
+			escribehistoria ('la app se reinicio');
 		},false);//resume
 		document.addEventListener("online",function(){
 			escribehistoria('la app se conecto a la red');
@@ -25,6 +25,12 @@ $(document).ready(function(e){
 	
 	
 });//document
+
+function escribehistoria(accion){
+	$('#eHistoria').append('<li>'+accion+'</li>');
+}
+  
+
 
 function escribehistoria(accion){
 	$('#eHistoria').append('<li>'+accion+'</li>');
